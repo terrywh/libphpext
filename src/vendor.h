326184f0d1bd67276016bee2ae41e6322a38013b
terrywh <terry.wuhao@gmail.com>
2017-01-12 19:26:22 +0800

    依赖库统一头文件

---
+++ src/vendor.h
@@ -0,0 +1,16 @@
+#ifndef PHPEXT_VENDOR_H
+#define PHPEXT_VENDOR_H
+
+#include <exception>
+#include <string>
+
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+
+using std::isnan;
+#include <zend.h>
+#include <zend_API.h>
+
+#endif // PHPEXT_VENDOR_H
