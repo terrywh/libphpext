9df60ca0bd8c40a5f1292f71f0de040cd605b427
terrywh <terry.wuhao@gmail.com>
2017-04-24 11:03:37 +0800

    补充强烈注释说明

--- bases/class_base.h
+++ bases/class_base.h
@@ -6,6 +6,7 @@ namespace php {
 		class_base();
 		class_base(const class_base& base);
 		value prop(const char* name);
+		// !!! prop 更新设置属性必须已经提前“声明”，否则会导致位置错误
 		class_base& prop(const char* name, const value& v);
 		inline void _object_set(zend_object* obj) {
 			ZVAL_OBJ(&val_, obj);
