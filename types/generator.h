177f96d186f6e5754c11b54d43d15be15be3305a
terrywh <terry.wuhao@gmail.com>
2017-05-16 12:25:16 +0800

    允许构建空 generator 对象；

--- types/generator.h
+++ types/generator.h
@@ -4,6 +4,7 @@ namespace php {
 	class value;
 	class generator : public value {
 	public:
+		generator(): value() {};
 		generator(const generator& obj): value(obj) {}
 		generator(generator&& obj): value(std::move(obj)) {}
 
@@ -13,5 +14,7 @@ namespace php {
 		void throw_exception(const php::value& e);
 		void throw_exception(const std::string& msg, int code = 0);
 		bool valid();
+
+		using value::operator=;
 	};
 }
