d8d4918d45c4411c00b75d993e09f6f90d6b3bd1
wuhao <terry.wuhao@gmail.com>
2018-05-25 13:54:58 +0800

    修正: 通过 PHP 调用 __closure (直接调用 __closure::fn_ 似乎会引起某种内存问题);

--- types/callable.cpp
+++ types/callable.cpp
@@ -3,12 +3,8 @@
 namespace php {
 	value callable::__invoke(zval* cb, int argc, zval* argv, bool silent) {
 		value rv;
-		if(Z_OBJCE_P(cb) == class_entry<class_closure>::entry()) {
-			// 内置的 closure 对象，简化调用过程以提高效率
-			class_closure* c = class_wrapper<class_closure>::from_obj(Z_OBJ_P(cb));
-			parameters params(argc, argv);
-			rv = c->fn_(params);
-		}else if(FAILURE == call_user_function_ex(EG(function_table), nullptr, cb, (zval*)&rv, argc, argv, 1, nullptr) && !silent) {
+		// 简化直接调用 __closure::fn_ 似乎会导致某种内存问题, 原因不明...
+		if(FAILURE == call_user_function_ex(EG(function_table), nullptr, cb, (zval*)&rv, argc, argv, 1, nullptr) && !silent) {
 			zend_error_noreturn(E_USER_ERROR, "failed to invoke callable '%s'", zval_get_string(cb));
 		}
 		return std::move(rv);
