bd19ffe8968317476b6ba3649f9f140e493cee1f
wuhao <terry.wuhao@gmail.com>
2018-05-25 13:45:59 +0800

    优化: 简化 generator 代码, 直接调用 PHP 内置函数, 防止出现版本兼容问题;

--- types/generator.cpp
+++ types/generator.cpp
@@ -1,122 +1,44 @@
 #include "../phpext.h"
 
 namespace php {
-	// 代码来自 php 源码 zend/zend_generators.c 相关位置
-	static void zend_generator_ensure_initialized(zend_generator *generator) {
-		if (UNEXPECTED(Z_TYPE(generator->value) == IS_UNDEF) && EXPECTED(generator->execute_data) && EXPECTED(generator->node.parent == NULL)) {
-			generator->flags |= ZEND_GENERATOR_DO_INIT;
-			zend_generator_resume(generator);
-			generator->flags &= ~ZEND_GENERATOR_DO_INIT;
-			generator->flags |= ZEND_GENERATOR_AT_FIRST_YIELD;
-		}
+	void generator::rewind() {
+		reinterpret_cast<php::object*>(this)->call(std::string("rewind",6));
 	}
-	
-	// 代码来自 php 源码 zend/zend_generators.c 相关位置
-	php::value generator::current() {
-		zend_generator* gen_ = reinterpret_cast<zend_generator*>(Z_OBJ(value_));
-		zend_generator_ensure_initialized(gen_);
-		zend_generator* root = zend_generator_get_current(gen_);
-		if (EXPECTED(gen_->execute_data != NULL && Z_TYPE(root->value) != IS_UNDEF)) {
-			zval *value = &root->value;
-			ZVAL_DEREF(value);
-			// ZVAL_COPY(return_value, value);
-			return *value;
-		}
-		return nullptr;
-	}
-	// 代码来自 php 源码 zend/zend_generators.c 相关位置
-	void generator::next() {
-		zend_generator* gen_ = reinterpret_cast<zend_generator*>(Z_OBJ(value_));
-		zend_generator_ensure_initialized(gen_);
-		zend_generator_resume(gen_);
-	}
-	// 代码来自 php 源码 zend/zend_generators.c 相关位置
-	php::value generator::send(const php::value& v) {
-		zend_generator* gen_ = reinterpret_cast<zend_generator*>(Z_OBJ(value_)),
-			*root;
-		zend_generator_ensure_initialized(gen_);
-		/* The generator is already closed, thus can't send anything */
-		if (UNEXPECTED(!gen_->execute_data)) {
-			return nullptr;
-		}
-		root = zend_generator_get_current(gen_);
-		/* Put sent value in the target VAR slot, if it is used */
-		if (root->send_target) {
-			ZVAL_COPY(root->send_target, (zval*)&v);
-		}
-		zend_generator_resume(gen_);
-		root = zend_generator_get_current(gen_);
-		if (EXPECTED(gen_->execute_data)) {
-			zval *val = &root->value;
-			ZVAL_DEREF(val);
-			// ZVAL_COPY(return_value, value);
-			return *val;
-		}
-		return nullptr;
-	}
-	// 代码来自 php 源码 zend/zend_generators.c 相关位置
-	static void zend_generator_throw_exception(zend_generator *generator, zval* exception) {
-		zend_execute_data *original_execute_data = EG(current_execute_data);
-		/* if we don't stop an array/iterator yield from, the exception will only reach the generator after the values were all iterated over */
-		if (UNEXPECTED(Z_TYPE(generator->values) != IS_UNDEF)) {
-			zval_ptr_dtor(&generator->values);
-			ZVAL_UNDEF(&generator->values);
-		}
-		EG(current_execute_data) = generator->execute_data;
-		generator->execute_data->opline--;
-		// 异步流程修正错误发生的位置(注意这里需要使用基类类型)
-		if(instanceof_function(Z_OBJCE_P(exception), zend_ce_throwable)) {
-			zend_update_property_string(
-				zend_get_exception_base(exception), exception, "file", sizeof("file")-1, zend_get_executed_filename());
-			zend_update_property_long(
-				zend_get_exception_base(exception), exception, "line", sizeof("line")-1, zend_get_executed_lineno());
-		}
-		zend_throw_exception_object(exception);
-		generator->execute_data->opline++;
-		EG(current_execute_data) = original_execute_data;
-	}
-	// 代码流程来自 php 源码 zend/zend_generators.c 相关位置
-	php::value generator::throw_exception(const php::value& ex) {
-		zend_generator* gen_ = reinterpret_cast<zend_generator*>(Z_OBJ(value_)),
-			*root;
-		zval exception;
-		ZVAL_DUP(&exception, const_cast<zval*>(reinterpret_cast<const zval*>(&ex)));
-		zend_generator_ensure_initialized(gen_);
-		if (gen_->execute_data) {
-			root = zend_generator_get_current(gen_);
-			zend_generator_throw_exception(root, &exception);
-			zend_generator_resume(gen_);
-			root = zend_generator_get_current(gen_);
-			if (gen_->execute_data) {
-				zval *val = &root->value;
-				ZVAL_DEREF(val);
-				// ZVAL_COPY(return_value, value);
-				return *val;
-			}
-		} else {
-			zend_throw_exception_object(&exception);
-		}
-		return nullptr;
-	}
-	
-	// 代码来自 php 源码 zend/zend_generators.c 相关位置
 	bool generator::valid() {
-		zend_generator* gen_ = reinterpret_cast<zend_generator*>(Z_OBJ(value_));
-		zend_generator_ensure_initialized(gen_);
-		zend_generator_get_current(gen_);
-		return EXPECTED(gen_->execute_data != NULL);
+		return reinterpret_cast<php::object*>(this)->call(std::string("valid",5)).to_bool();
 	}
-
+	php::value generator::current() {
+		return reinterpret_cast<php::object*>(this)->call(std::string("current",7));
+	}
+	php::value generator::key() {
+		return reinterpret_cast<php::object*>(this)->call(std::string("key",3));
+	}
+	void generator::next() {
+		reinterpret_cast<php::object*>(this)->call(std::string("next",4));
+	}
+	php::value generator::send(const php::value& v) {
+		return reinterpret_cast<php::object*>(this)->call(std::string("send",4), {v});
+	}
+	php::value generator::throw_exception(const std::string& ex, int code) {
+		return throw_exception( php::object::create_exception(ex, code) );
+	}
+	php::value generator::throw_exception(php::value ex) {
+		// 异步流程修正错误发生的位置 (注意这里需要使用基类类型)
+		zend_execute_data *original_execute_data = EG(current_execute_data);
+		zend_generator* g = (zend_generator*)Z_OBJ_P(&value_);
+		EG(current_execute_data) = g->execute_data;
+		g->execute_data->opline--;
+		// 异步流程修正错误发生的位置(注意这里需要使用基类类型)
+		if(static_cast<php::object&>(ex).is_instance_of(zend_ce_throwable)) {
+			static_cast<php::object&>(ex).prop("file",4) = php::string(zend_get_executed_filename());
+			static_cast<php::object&>(ex).prop("line",4) = zend_get_executed_lineno();
+		}
+		g->execute_data->opline++;
+		EG(current_execute_data) = original_execute_data;
+		
+		return reinterpret_cast<php::object*>(this)->call(std::string("throw",5), {ex});
+	}
 	php::value generator::get_return() {
-		zend_generator *gen_ = reinterpret_cast<zend_generator*>(Z_OBJ(value_));
-		zend_generator_ensure_initialized(gen_);
-		if (UNEXPECTED(EG(exception))) {
-			return nullptr;
-		}
-		if(Z_ISUNDEF(gen_->retval)) {
-			throw php::exception("connot get return value of a generator that hasn't returned", 0);
-			// return nullptr;
-		}
-		return gen_->retval;
+		return reinterpret_cast<php::object*>(this)->call(std::string("getReturn",9));
 	}
 }
--- types/generator.h
+++ types/generator.h
@@ -12,15 +12,16 @@ namespace php {
 		generator():value() {}
 		generator(const php::value& v): value(v) {}
 		generator(php::value&& v): value(std::move(v)) {}
-
+		
+		
+		void rewind();
+		bool valid();
 		php::value current();
+		php::value key();
 		void next();
 		php::value send(const php::value& v);
-		php::value throw_exception(const php::value& ex);
-		inline php::value throw_exception(const std::string& msg, int code = 0) {
-			return throw_exception(php::object::create_exception(msg, code));
-		}
-		bool valid();
+		php::value throw_exception(const std::string& ex, int code = 0);
+		php::value throw_exception(php::value ex);
 		php::value get_return();
 	};
 }
